17418b3a706c954d860aa2b2cac78eebbd041adb
Kevin Phan <ken.phan@live.ca>
2021-10-23 14:27:45 -0700

    add pizza toppings list and price per pizza slice

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 toppings = [
-  "pepperoni"
-  "pineapple"
-  "cheese"
-  "sausage"
-  "olives"
-  "anchovies"
+  "pepperoni",
+  "pineapple",
+  "cheese",
+  "sausage",
+  "olives",
+  "anchovies",
   "mushrooms"
 ]
+
+prices = [2, 6, 1, 3, 2, 7, 2]
