88b154d4e23530ed4bcd5f290bc2e552b3ebbdc6
Kevin Phan <ken.phan@live.ca>
2021-10-23 19:52:31 -0700

    print pizza_and_prices

--- script.py
+++ script.py
@@ -15,3 +15,6 @@ print(num_two_dollar_slices)
 
 num_pizzas = len(toppings)
 print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+pizza_and_prices = [ [2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+print(pizza_and_prices)
