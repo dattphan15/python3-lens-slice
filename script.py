5530d5cd0a890f8d9c3d0ea5cf0f78ff8c229a32
Kevin Phan <ken.phan@live.ca>
2021-10-23 20:10:54 -0700

    completes list of pizza

--- script.py
+++ script.py
@@ -11,27 +11,41 @@ toppings = [
 prices = [2, 6, 1, 3, 2, 7, 2]
 
 num_two_dollar_slices = prices.count(2)
-print(num_two_dollar_slices)
+print("We have " + str(num_two_dollar_slices) + " types of $2 slice pizzas")
+print()
 
 num_pizzas = len(toppings)
 print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+print()
 
 pizza_and_prices = [ [2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+print("Original list of toppings")
 print(pizza_and_prices)
+print()
 
 pizza_and_prices.sort()
+print("Sorted list of toppings")
 print(pizza_and_prices)
+print()
 
 cheapest_pizza = pizza_and_prices[0]
+print("Our cheapest pizza")
 print(cheapest_pizza)
+print()
 
 priciest_pizza = pizza_and_prices[-1]
+print("Our priciest pizza")
 print(priciest_pizza)
+print()
 
 pizza_and_prices.pop()
 
 pizza_and_prices.insert(3, [2.5, "peppers"])
+print("Our new pizza menu with our added pepper topping")
 print(pizza_and_prices)
+print()
 
 three_cheapest = pizza_and_prices[:3]
+print("Our 3 cheapest pizzas")
 print(three_cheapest)
+print()
