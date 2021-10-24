01c9074af6349674d732be276e6e2586a69beeed
Kevin Phan <ken.phan@live.ca>
2021-10-23 19:42:58 -0700

    print num_pizzas in string

--- script.py
+++ script.py
@@ -14,4 +14,4 @@ num_two_dollar_slices = prices.count(2)
 print(num_two_dollar_slices)
 
 num_pizzas = len(toppings)
-print(num_pizzas)
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
